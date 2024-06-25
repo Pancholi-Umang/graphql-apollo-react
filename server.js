c60e376df97df2ebcea0437f259da6814ff9763d
umang pancholi <umangpancholi555@gmail.com>
2024-06-26 00:19:12 +0530

    create queries and resolvers for users and quotes

--- server.js
+++ server.js
@@ -1,29 +1,56 @@
-import {ApolloServer,gql} from 'apollo-server'
-import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core'
+import { ApolloServer, gql } from 'apollo-server'
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core'
+import { quotes, users } from './fakedb.js'
 
 const typeDefs = gql`
    type Query{
-       greet:String
-   }
+       users:[User] 
+       quotes:[Quote]
+       user(id:ID!):User
+       quote(by:ID!):Quote
+   }  
+    type User{
+        id:ID
+        firstName:String
+        lastName:String
+        email:String
+        quotes:[Quote]
+    }
+    type Quote{
+        name:String
+        by:ID
+        users:[User]
+    }
 `;
 
 const resolvers = {
-    Query:{
-        greet:()=>{
-            return "Hello world"
-        }
+    Query: {
+        users: () => users,
+        quotes: () => quotes,
+        user:(_,{id})=>users?.find((user)=>user?.id === id),
+        quote:(_,{by})=>quotes?.find((quote)=>quote?.by === by)
+    },
+
+    // Filter Quote by users (user ni andar quote jota chhe atle)
+    User:{
+        quotes:(singleUser)=>quotes.filter((singleQuote)=>singleUser?.id === singleQuote?.by),
+    },
+
+    // Filter User by quotes (Quote ni andar User ni detail joti chhe atle)
+    Quote:{
+        users:(singleQuote)=>users.filter((singleUser)=>singleUser?.id === singleQuote?.by),
     }
 }
 
 
-const server = new ApolloServer({ 
-    typeDefs, 
+const server = new ApolloServer({
+    typeDefs,
     resolvers,
-    plugins:[
+    plugins: [
         ApolloServerPluginLandingPageGraphQLPlayground()
     ]
 });
 
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+    console.log(`🚀  Server ready at ${url}`);
 });
